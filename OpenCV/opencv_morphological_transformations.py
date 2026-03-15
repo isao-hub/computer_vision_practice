53386628174adc774bdf4946ee165d7dffcc7616
Isao Ishikawa <isaoishikawa@Isaos-MacBook-Air.local>
2026-03-15 13:30:21 +0900

    xxx

--- OpenCV/opencv_morphological_transformations.py
+++ OpenCV/opencv_morphological_transformations.py
@@ -11,14 +11,14 @@ filename = "beard1.jpeg"
 # パス結合
 image_path = os.path.join(IMAGE_DIR, filename)
 
-# 画像読込み
+# 画像読込み (OpenCVではグレースケールに対する処理)
 img = cv.imread(image_path, cv.IMREAD_GRAYSCALE)
 
 if img is None: 
     print("エラー：画像が読み込めませんでした")
     exit()
 
-kernel = np.ones((5,5), np.unit8)
+kernel = np.ones((5,5), np.uint8)
 
 # 1. Erosion
 erosion = cv.erode(img, kernel, iterations=1)
@@ -27,10 +27,29 @@ dilation = cv.dilate(img, kernel, iterations=1)
 # 3. Opening
 opening = cv.morphologyEx(img, cv.MORPH_OPEN,kernel)
 # 4. Closing
-cloasing = cv.morphologyEx(img, cv.MORPH_CLOSE,kernel)
+closing = cv.morphologyEx(img, cv.MORPH_CLOSE,kernel)
 # 5. Morphological Gradient
 gradient = cv.morphologyEx(img, cv.MORPH_GRADIENT,kernel)
 # 6. Top Hat
 tophat = cv.morphologyEx(img, cv.MORPH_TOPHAT,kernel)
 # 7. Black Hat
 blackhat = cv.morphologyEx(img, cv.MORPH_BLACKHAT,kernel)
+
+# 可視化
+titles = ["Original", 'Erosion', 
+          'Dilation', 'Opening', 
+          'Closing', 'Gradient',
+          'Top Hat', 'Black Hat']
+images = [img, erosion, 
+          dilation, opening, 
+          closing, gradient, 
+          tophat, blackhat]
+
+plt.figure(figsize=(12,8))
+for i in range(8):
+    plt.subplot(2, 4, i+1)
+    plt.imshow(images[i], "gray")
+    plt.title(titles[i])
+    plt.xticks([]), plt.yticks([]) #目盛り値の設定オフ
+plt.tight_layout()
+plt.show()
